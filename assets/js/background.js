d7d882aab00edc243c73335aa71adeb4252dad77
tauqir-rabbani <tauqir.pbhub003@gmail.com>
2024-04-09 17:34:06 +0500

    j

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,4 +1,4 @@
-let receive = '';
+let receive = "";
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.data) {
     receive = message.data;
@@ -6,6 +6,25 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-function test(){
+function test() {
   console.log(receive);
+  if(receive == 'google'){
+    // toggleIcon()
+  }
 }
+
+console.log(chrome.action.setIcon({path: {"128": "assets/images/logo2.png"}}));
+
+function toggleIcon() {
+  chrome.browserAction.getIcon(function(currentIcon) {
+      var newIcon = currentIcon == "assets/images/logo.png" ? "assets/images/logo2.png" : "assets/images/logo.png";
+      chrome.browserAction.setIcon({ path: newIcon });
+  });
+}
+
+// // Example: Listen for a message from a content script or popup
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   if (message.action == "toggleIcon") {
+//     toggleIcon();
+//   }
+// });
